56f368f99d491de757b235eb55dafb683b67d7b9
alvaroseba2000@gmail.com <lvaro@DESKTOP-DQ95APK>
2021-08-19 22:17:39 -0400

    Agregar archivos de proyecto.

---
+++ PrototipoOracleAPIFull/DTO/RegionDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PrototipoOracleAPIFull.Models;
+
+namespace PrototipoOracleAPIFull.DTO
+{
+    public partial class RegionDTO
+    {
+        public decimal RegionId { get; set; }
+        public string RegionName { get; set; }
+
+        public RegionDTO()
+        {
+
+        }
+
+        public RegionDTO(Models.Region region)
+        {
+            this.RegionId = region.RegionId;
+            this.RegionName = region.RegionName;
+        }
+    }
+}
---
+++ PrototipoOracleAPIFull/Models/Job.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PrototipoOracleAPIFull.Models
+{
+    public partial class Job
+    {
+        public Job()
+        {
+            Employees = new HashSet<Employee>();
+            JobHistories = new HashSet<JobHistory>();
+        }
+
+        public string JobId { get; set; }
+        public string JobTitle { get; set; }
+        public int? MinSalary { get; set; }
+        public int? MaxSalary { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+        public virtual ICollection<JobHistory> JobHistories { get; set; }
+    }
+}
